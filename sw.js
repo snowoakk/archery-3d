33700fcbcdb1eaf525758caa3173880a5f933d5c
Sadudee Poaliwat <SadudeePoaliwat@gmail.com>
2025-12-02 21:38:46 +0700

    feat: add PWA

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'archery-3d-cache-v1';
+const urlsToCache = [
+  './archery.html',
+  './three.min.js',
+  './cannon.min.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install the service worker and cache the game assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
